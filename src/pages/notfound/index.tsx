f3cd657d9f842ea4b572aff46fdb689d5bdc0144
Fabiano Freitas <fabiano.freitas@gmail.com>
2024-04-13 15:27:05 -0300

    alteracao notfound.module.css

--- src/pages/notfound/index.tsx
+++ src/pages/notfound/index.tsx
@@ -1,10 +1,10 @@
-
+import styles from './notfound.module.css';
 
 export  function Notfound() {
     
     return (
-      <div>
-          <h1>Pagina 404, esta pagina nao existe</h1>
+      <div className={styles.container}>
+          <h1 className={styles.h1}>Pagina 404, esta pagina nao existe</h1>
       </div>
     )
   }
